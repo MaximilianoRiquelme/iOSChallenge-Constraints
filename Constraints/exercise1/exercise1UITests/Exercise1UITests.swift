770bc8bbb381542d487d09f6a08f4af0bf136e67
Maximiliano Riquelme Vera <maximiliano.riquelme@globant.com>
2021-07-23 20:58:17 -0300

    Test types capitalized

--- Constraints/exercise1/exercise1UITests/Exercise1UITests.swift
+++ Constraints/exercise1/exercise1UITests/Exercise1UITests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-class exercise1UITests: XCTestCase {
+class Exercise1UITests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
