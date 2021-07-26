039f07b415f7c846b2dba1b5ad235c39ce46d460
Maximiliano Riquelme Vera <maximiliano.riquelme@globant.com>
2021-07-26 15:51:39 -0300

    added corrections on types names

--- Constraints/exercise2/exercise2Tests/exercise2Tests.swift
+++ Constraints/exercise2/exercise2Tests/exercise2Tests.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import exercise2
 
-class exercise2Tests: XCTestCase {
+class Exercise2Tests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
