be76f779719a686f791b1376fee2d8333999a4ff
Maximiliano Riquelme Vera <maximiliano.riquelme@globant.com>
2021-07-27 13:34:59 -0300

    working on it

---
+++ Constraints/exercise3/exercise3/Message.swift
@@ -0,0 +1,17 @@
+//
+//  Message.swift
+//  exercise3
+//
+//  Created by Maximiliano Riquelme Vera on 27/07/2021.
+//
+
+import Foundation
+
+class Message {
+    
+    let username : String = ""
+    let message : String = ""
+    let timeMessage : String = ""
+    
+    
+}
--- Constraints/exercise3/exercise3/ViewController.swift
+++ Constraints/exercise3/exercise3/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var board_tableView: UITableView!
+    
+    //let messages = [Message]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        board_tableView.delegate = self
+        board_tableView.dataSource = self
     }
-
-
+    
+    //Called when a cell is selected
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // Do something perhaps
+    }
+    
+    //Sets the amount of rows on the table
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5 //ammount of rows
+    }
+    
+    //
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "sended", for: indexPath)
+        
+        //Change the cell somehow.
+    
+        return cell
+    }
 }
 
